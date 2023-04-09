37154c4c8c47dee89419a75f4d63bddc69749f25
xiaohaiz <xiaohaiz@hotmail.com>
2023-04-09 16:27:05 +0800

    Import js-cookit.

--- pocketrose.js
+++ pocketrose.js
@@ -9,6 +9,7 @@
 // @grant        unsafeWindow
 // @match        *://pocketrose.itsns.net.cn/*
 // @require      https://code.jquery.com/jquery-2.1.4.min.js
+// @require      https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/dist/js.cookie.min.js
 // @run-at       document-start
 // @unwrap
 // ==/UserScript==
