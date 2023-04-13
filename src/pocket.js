a5bfb91706b639d78fc2966ba05d3965a23ef11c
xiaohaiz <xiaohaiz@hotmail.com>
2023-04-13 22:54:03 +0800

    Town

--- src/pocket.js
+++ src/pocket.js
@@ -5,3 +5,41 @@
  */
 
 export const DOMAIN = "https://pocketrose.itsns.net.cn/pocketrose";
+
+/**
+ * 口袋城市数据结构
+ */
+export class Town {
+
+    #id;
+    #name;
+    #description;
+    #coordinate;
+
+    constructor(id, name, description, coordinate) {
+        this.#id = id;
+        this.#name = name;
+        this.#description = description;
+        this.#coordinate = coordinate;
+    }
+
+    get id() {
+        return this.#id;
+    }
+
+    get name() {
+        return this.#name;
+    }
+
+    get description() {
+        return this.#description;
+    }
+
+    get coordinate() {
+        return this.#coordinate;
+    }
+
+    longText() {
+        return "(" + this.#id + ") " + this.#name + " " + this.#coordinate.longText();
+    }
+}
