da48100973f485ff43f7695ba59a9f6ab36c48b1
xiaohaiz <xiaohaiz@hotmail.com>
2023-04-13 21:12:54 +0800

    getAllCastles

--- src/castle.js
+++ src/castle.js
@@ -4,6 +4,9 @@
  * ============================================================================
  */
 
+import * as map from "./map";
+import * as network from "./network";
+
 /**
  * 城堡的数据结构
  */
@@ -42,4 +45,30 @@ export class Castle {
     get coordinate() {
         return this.#coordinate;
     }
+}
+
+/**
+ * 读取所有的城堡信息并回调。
+ * @param callback 回调函数
+ */
+export function getAllCastles(callback) {
+
+    network.sendGetRequest("", function (html) {
+
+        const castles = {};
+
+        $(html).find("td").each(function (_idx, td) {
+            const text = $(td).text();
+            if (text.endsWith(" (自购)")) {
+                const name = $(td).prev().text();
+                const owner = text.substring(0, text.indexOf(" (自购)"));
+                const location = $(td).next().text();
+                const parsedLocation = location.substring(1, location.length - 1).split(",");
+                const coordinate = new map.Coordinate(parseInt(parsedLocation[0]), parseInt(parsedLocation[1]));
+                castles[owner] = new Castle(name, owner, location);
+            }
+        });
+
+        callback(castles, {"html": html});
+    });
 }
