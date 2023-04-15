3713fa19e5271b717b368a98feca44d02aa618b1
xiaohaiz <xiaohaiz@hotmail.com>
2023-04-15 20:30:54 +0800

    Move castle related functions to user.

--- src/castle.js
+++ src/castle.js
@@ -6,87 +6,13 @@
 
 import * as map from "./map";
 import {enterTown, leaveCastle} from "./map";
-import * as network from "./network";
 import * as page from "./page";
 import {generateCredential} from "./page";
 import * as pocket from "./pocket";
 import * as util from "./util";
-import {Coordinate} from "./geo";
 import * as finance from "./bank";
 import * as user from "./user";
 import {createEventHandler} from "./event";
-
-/**
- * 城堡的数据结构
- */
-export class Castle {
-
-    #name;              // 城堡名字
-    #owner;             // 城堡主人
-    #coordinate;        // 城堡坐标
-
-    constructor(name, owner, coordinate) {
-        this.#name = name;
-        this.#owner = owner;
-        this.#coordinate = coordinate;
-    }
-
-    /**
-     * Get castle name.
-     * @returns {string}
-     */
-    get name() {
-        return this.#name;
-    }
-
-    /**
-     * Get castle owner.
-     * @returns {string}
-     */
-    get owner() {
-        return this.#owner;
-    }
-
-    /**
-     * Get castle _coordinate
-     * @returns {map.Coordinate}
-     */
-    get coordinate() {
-        return this.#coordinate;
-    }
-
-    longText() {
-        return this.#name + "/" + this.#owner + "/" + this.#coordinate.longText();
-    }
-}
-
-/**
- * 读取所有的城堡信息并回调。
- * @param callback 回调函数
- */
-export function getAllCastles(callback) {
-
-    network.sendGetRequest("castle_print.cgi", function (html) {
-
-        const castles = {};
-
-        $(html).find("td").each(function (_idx, td) {
-            const text = $(td).text();
-            if (text.endsWith(" (自购)")) {
-                const name = $(td).prev().text();
-                const owner = text.substring(0, text.indexOf(" (自购)"));
-                let location = $(td).next().text();
-                location = util.substringBetween(location, "(", ")");
-                let x = util.substringBefore(location, ",");
-                let y = util.substringAfter(location, ",");
-                const coordinate = new Coordinate(parseInt(x), parseInt(y));
-                castles[owner] = new Castle(name, owner, coordinate);
-            }
-        });
-
-        callback(castles, {"html": html});
-    });
-}
 
 /**
  * 城堡相关页面的处理入口
--- src/user.js
+++ src/user.js
@@ -8,6 +8,7 @@
  */
 
 import * as geo from "./geo";
+import {Coordinate} from "./geo";
 import * as network from "./network";
 import * as pocket from "./pocket";
 import * as util from "./util";
@@ -306,4 +307,76 @@ export async function loadRole(credential) {
         });
     };
     return await doLoadRole(credential);
+}
+
+/**
+ * 城堡的数据结构
+ */
+export class Castle {
+
+    #name;              // 城堡名字
+    #owner;             // 城堡主人
+    #coordinate;        // 城堡坐标
+
+    constructor(name, owner, coordinate) {
+        this.#name = name;
+        this.#owner = owner;
+        this.#coordinate = coordinate;
+    }
+
+    /**
+     * Get castle name.
+     * @returns {string}
+     */
+    get name() {
+        return this.#name;
+    }
+
+    /**
+     * Get castle owner.
+     * @returns {string}
+     */
+    get owner() {
+        return this.#owner;
+    }
+
+    /**
+     * Get castle _coordinate
+     * @returns {map.Coordinate}
+     */
+    get coordinate() {
+        return this.#coordinate;
+    }
+
+    longText() {
+        return this.#name + "/" + this.#owner + "/" + this.#coordinate.longText();
+    }
+}
+
+/**
+ * 读取所有的城堡信息并回调。
+ * @param callback 回调函数
+ */
+export function getAllCastles(callback) {
+
+    network.sendGetRequest("castle_print.cgi", function (html) {
+
+        const castles = {};
+
+        $(html).find("td").each(function (_idx, td) {
+            const text = $(td).text();
+            if (text.endsWith(" (自购)")) {
+                const name = $(td).prev().text();
+                const owner = text.substring(0, text.indexOf(" (自购)"));
+                let location = $(td).next().text();
+                location = util.substringBetween(location, "(", ")");
+                let x = util.substringBefore(location, ",");
+                let y = util.substringAfter(location, ",");
+                const coordinate = new Coordinate(parseInt(x), parseInt(y));
+                castles[owner] = new Castle(name, owner, coordinate);
+            }
+        });
+
+        callback(castles);
+    });
 }
