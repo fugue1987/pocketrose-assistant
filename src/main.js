19312e8c32e99bf9c4fb53a04d188bfa67268b75
xiaohaiz <xiaohaiz@hotmail.com>
2023-04-15 18:42:00 +0800

    删除不再需要的函数（已经迁移）。

--- src/main.js
+++ src/main.js
@@ -77,9 +77,6 @@ function replacePkm(page) {
             if (__cookie_getEnablePokemonWiki()) {
                 pokemon.processPokemonWikiReplacement();
             }
-            if (location.href.includes("/status.cgi")) {
-                postProcessMainStatusFunctionalities($('html').html());
-            }
             if (location.href.includes("/town.cgi")) {
                 postProcessCityRelatedFunctionalities($('html').html());
             }
@@ -523,89 +520,11 @@ function __common_item_selectAllStorableItems(parentElement) {
     return checkedCount;
 }
 
-// ============================================================================
-// 主状态页辅助功能
-// ============================================================================
-function postProcessMainStatusFunctionalities(htmlText) {
-    if (htmlText.indexOf("在网吧的用户请使用这个退出") !== -1) {
-        //__status(htmlText);
-    }
-}
-
-function __status(htmlText) {
-    $("option[value='INN']").text("客栈·驿站");
-    $("option[value='LETTER']").text("口袋助手设置");
-    $("option[value='LETTER']").attr("style", "background:#20c0ff");
-    $("option[value='CHANGEMAP']").text("冒险家公会");
-
-    // 主页面如果角色满级则经验(大于等于14900)显示为蓝色
-    $("td:parent").each(function (_idx, td) {
-        const text = $(td).text();
-        if (text === "经验值") {
-            const expText = $(td).next().text();
-            const exp = expText.substring(0, expText.indexOf(" EX"));
-            if (exp >= 14900) {
-                let expHtml = $(td).next().html();
-                expHtml = "<font color='blue'>" + expHtml + "</font>";
-                $(td).next().html(expHtml);
-            }
-        }
-    });
-
-    // 读取角色当前的能力值
-    const text = $("#c_001").find("table:last").find("td:first").text();
-    let idx = text.indexOf("Lv：");
-    let s = text.substring(idx);
-    const level = parseInt(s.substring(3, s.indexOf(" ")));
-    idx = text.indexOf("攻击力：");
-    s = text.substring(idx);
-    const attack = parseInt(s.substring(4, s.indexOf(" ")));
-    idx = s.indexOf("防御力：");
-    s = s.substring(idx);
-    const defense = parseInt(s.substring(4, s.indexOf(" ")));
-    idx = s.indexOf("智力：");
-    s = s.substring(idx);
-    const specialAttack = parseInt(s.substring(3, s.indexOf(" ")));
-    idx = s.indexOf("精神力：");
-    s = s.substring(idx);
-    const specialDefense = parseInt(s.substring(4, s.indexOf(" ")));
-    idx = s.indexOf("速度：");
-    s = s.substring(idx);
-    const speed = parseInt(s.substring(3));
-    if (level !== 150 && (attack === 375 || defense === 375 || specialAttack === 375 || specialDefense === 375 || speed === 375)) {
-        $("td:parent").each(function (_idx, td) {
-            const text = $(td).text();
-            if (text === "身份") {
-                let titleHtml = $(td).next().html();
-                titleHtml = "<font color='red'>" + titleHtml + "</font>";
-                $(td).next().html(titleHtml);
-            }
-        });
-    }
-
-    // 主页面如果角色现金超过100万显示为红色
-    $("td:parent").each(function (_idx, td) {
-        const text = $(td).text();
-        if (text === "资金") {
-            const cashText = $(td).next().text();
-            const cash = cashText.substring(0, cashText.indexOf(" Gold"));
-            if (cash >= 1000000) {
-                let cashHtml = $(td).next().html();
-                cashHtml = "<font color='red'>" + cashHtml + "</font>";
-                $(td).next().html(cashHtml);
-            }
-        }
-    });
-}
 
 // ============================================================================
 // 城市点击后续辅助功能
 // ============================================================================
 function postProcessCityRelatedFunctionalities(htmlText) {
-    if (htmlText.indexOf("在网吧的用户请使用这个退出") !== -1) {
-        // 战斗后的主页是通过town.cgi返回的
-        //__status(htmlText);
-    }
     if (htmlText.indexOf("* 宿 屋 *") !== -1) {
         __town_inn(htmlText);
     }
