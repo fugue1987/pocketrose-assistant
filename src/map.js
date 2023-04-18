3bc030d8f1e9d382eda0a6f511d15951cc1c008c
張曉海 <xiaohai.zhang@17zuoye.com>
2023-04-18 08:53:32 +0800

    initializeMovePlan

--- src/map.js
+++ src/map.js
@@ -5,6 +5,7 @@
  */
 
 import * as geo from "./geo";
+import {Coordinate} from "./geo";
 import * as network from "./network";
 import * as util from "./util";
 import * as message from "./message";
@@ -287,4 +288,43 @@ export async function explore(credential) {
         });
     };
     return await doExplore(credential);
+}
+
+/**
+ * 当进入地图模式时，读取地图的信息初始化移动计划。
+ * 有三种方式进入地图：
+ * 1. 出城
+ * 2. 离开城堡
+ * 3. 大地图子菜单中退出
+ * @param html 源HTML
+ * @returns {MovePlan}
+ */
+function initializeMovePlan(html) {
+    const scope = $(html).find("select[name='chara_m']")
+        .find("option:last").attr("value");
+    let mode = "ROOK";
+    $(html).find("input:submit").each(function (_idx, input) {
+        const v = $(input).attr("value");
+        const d = $(input).attr("disabled");
+        if (v === "↖" && d === undefined) {
+            mode = "QUEEN";
+        }
+    });
+    let from = undefined;
+    $(html).find("td").each(function (_idx, td) {
+        const text = $(td).text();
+        if (text.includes("现在位置(") && text.endsWith(")")) {
+            const s = util.substringBetween(text, "(", ")");
+            const x = util.substringBefore(s, ",");
+            const y = util.substringAfter(s, ",");
+            from = new Coordinate(parseInt(x), parseInt(y));
+        }
+    });
+    message.publishMessageBoard(message._message_move_mode, {"mode": mode});
+    message.publishMessageBoard(message._message_move_scope, {"scope": scope});
+    const plan = new MovePlan();
+    plan.scope = scope;
+    plan.mode = mode;
+    plan.source = from;
+    return plan;
 }
