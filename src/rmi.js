0075fc39367707480ed8f92db99d607a212eb5c6
xiaohaiz <xiaohaiz@hotmail.com>
2023-04-20 16:33:34 +0800

    consecratePet implementation.

---
+++ src/rmi.js
@@ -0,0 +1,30 @@
+/**
+ * ============================================================================
+ * [ 口 袋 远 程 调 用 模 块 ]
+ * ============================================================================
+ */
+
+import * as message from "./message";
+import * as network from "./network";
+
+/**
+ * 献祭宠物（超级封印）
+ * @param credential 用户凭证
+ * @param petIndex 宠物所在下标
+ * @returns {Promise<void>}
+ */
+export async function consecratePet(credential, petIndex) {
+    const doConsecratePet = function (credential, petIndex) {
+        return new Promise((resolve) => {
+            const request = credential.asRequest();
+            request["select"] = petIndex;
+            request["mode"] = "PETBORN6";
+            network.sendPostRequest("mydata.cgi", request, function (html) {
+                const successMessage = $(html).find("h2:first").text();
+                message.writeMessageBoard(successMessage);
+                resolve();
+            });
+        });
+    }
+    return await doConsecratePet(credential, petIndex);
+}
