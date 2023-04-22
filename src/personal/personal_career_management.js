48534a4546202e5e833700ea7d636511e07ddf20
張曉海 <xiaohai.zhang@17zuoye.com>
2023-04-22 12:09:07 +0800

    ...

--- src/personal/personal_career_management.js
+++ src/personal/personal_career_management.js
@@ -16,6 +16,24 @@ function doProcess() {
 
     const pageHTML = page.currentPageHTML();
     const careerCandidateList = doParseCareerCandidateList(pageHTML);
+
+    const credential = page.generateCredential();
+    $("table:first").removeAttr("height");
+    $("table:first tr:first")
+        .next().next().next()
+        .html("<td style='background-color:#F8F0E0'>" +
+            "<div id='CareerUI'></div><div id='Eden' style='display:none'></div>" +
+            "</td>");
+    $("table:first tr:first")
+        .next().next().next()
+        .after($("<tr><td style='background-color:#F8F0E0;text-align:center'>" +
+            "<form action='status.cgi' method='post'>" +
+            "<input type='hidden' name='id' value='" + credential.id + "'>" +
+            "<input type='hidden' name='pass' value='" + credential.pass + "'>" +
+            "<input type='hidden' name='mode' value='STATUS'>" +
+            "<input type='submit' value='返回城市'>" +
+            "</form>" +
+            "</td></tr>"));
 }
 
 function doParseCareerCandidateList(pageHTML) {
