26332cc26b5b4549e2a7d8d3d9ad92741753f229
xiaohaiz <xiaohaiz@hotmail.com>
2023-04-24 14:22:00 +0800

    购买物品实现。

--- src/town/town_super_market.js
+++ src/town/town_super_market.js
@@ -450,9 +450,8 @@ function __bindBuyButton() {
                 } else if (p1 === "ACC") {
                     __buyViaAccessoryStore(credential, townId, discount, p2, count);
                 } else if (p1 === "ITE") {
-
+                    __buyViaItemStore(credential, townId, discount, p2, count);
                 }
-
             });
     });
 }
@@ -501,6 +500,24 @@ function __buyViaAccessoryStore(credential, townId, discount, select, count) {
     request["select"] = select;
     request["num"] = count;
     request["mark"] = "2";
+    network.sendPostRequest("town.cgi", request, function (html) {
+        message.processResponseHTML(html);
+        service.depositIntoTownBank(credential, undefined)
+            .then(() => {
+                doRefresh(credential);
+                document.getElementById("PocketSuperMarketTitle").scrollIntoView();
+            });
+    });
+}
+
+function __buyViaItemStore(credential, townId, discount, select, count) {
+    const request = credential.asRequest();
+    request["val_off"] = discount;
+    request["townid"] = townId;
+    request["mode"] = "BUY";
+    request["select"] = select;
+    request["num"] = count;
+    request["mark"] = "3";
     network.sendPostRequest("town.cgi", request, function (html) {
         message.processResponseHTML(html);
         service.depositIntoTownBank(credential, undefined)
